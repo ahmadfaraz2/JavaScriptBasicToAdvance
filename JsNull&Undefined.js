eb08a6772daa4a79826e122a8062d5e7b439630f
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-06-18 23:41:52 +0500

    Add some information about 'null' and 'undefined'

--- JsNull&Undefined.js
+++ JsNull&Undefined.js
@@ -3,3 +3,12 @@ console.log(mVar);
 
 mVar = null; // mVar holds the value null
 console.log(mVar);
+
+
+
+// ---------------------Some Explanation about "undefined" and "null"---------------------------
+
+// So "undefined" means a variable has been declared but has not been initialized or not
+// yet been assigned a value.
+
+// "null" is a value assigned to a variable. Maybe just to represent that the variable holds no value.

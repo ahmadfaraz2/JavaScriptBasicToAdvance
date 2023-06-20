933f897a852eac72a8d8f21d00834da2387a68b8
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-06-20 11:12:36 +0500

    Arithmetic Operators - Deep dive into Increment and Decrement Post and pre fixes.

---
+++ JsArithmeticOperations.js
@@ -0,0 +1,39 @@
+var num1 = 10;
+var num2 = 15;
+var num3 = 20;
+var num4 = 30;
+
+//Addition
+// console.log(num1 + num2)
+// console.log(num1 + num2 + num3)
+
+//Subtraction
+// console.log(num2 - num1)
+// console.log(num3 - num2 - num1)
+
+//Multiplication
+// console.log(num1 * num2)
+
+//Division
+// console.log(num2 / num1);
+
+//Modulus
+// console.log(num2 % num1);
+
+console.log("Increment - Post and Pre");
+//Increment - Post and Pre
+console.log(num1++);
+console.log(num1);
+
+// Pre
+console.log(++num2);
+console.log(num2);
+
+console.log("Decrement - Post and Pre")
+//Decrement - Post and Pre
+console.log(num3--);
+console.log(num3);
+
+//Pre
+console.log(--num4);
+console.log(num4);

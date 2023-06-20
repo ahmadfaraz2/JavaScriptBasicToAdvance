22a8db51920ff9a5dcc35d730dbe719d4e0f16de
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-06-20 22:16:59 +0500

    Syntax of while loop and definition

---
+++ JsWhileLoop.js
@@ -0,0 +1,14 @@
+// A while loop iterates a block of code untill a
+// specified condition failed.
+
+// while(condition){
+// block of code 
+// }
+
+var mValue = 0;
+while (mValue < 50) {
+    mValue++;
+    console.log(mValue);
+}
+
+console.log(mValue);

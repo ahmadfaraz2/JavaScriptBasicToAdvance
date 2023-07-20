a9bfc107f4e8581ffb6a9186fa0f9a020b4a3197
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-07-20 18:08:56 +0500

    learn about querySelector and querySelectorAll

---
+++ DOM/query_selectors/index.js
@@ -0,0 +1,21 @@
+/*
+----------------Query Selectors----------------
+
+Query selectors allow you to use CSS selectors to select HTML
+elements. It is the new way in JavaScript to select HTML elements.
+
+               There are 2 such selectors:       
+                   querySelector()
+                   querySelectorAll()
+
+
+Remember:
+    Here you have to write dot(.) or hash(#) depending on whether you are 
+    workin with class or Id.                    
+*/
+
+//console.log(document.querySelectorAll(".blog-card img"));
+console.log(document.querySelectorAll("#topbar"));
+
+//console.log(document.querySelector(".blog-card img"));
+console.log(document.querySelector("#topbar"));

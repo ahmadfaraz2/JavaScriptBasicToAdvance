19d24967c5eea3240af74e10f98230e450bde15c
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-07-20 17:24:52 +0500

    introduction to DOM, also add ScreenShots

---
+++ DOM/intro_to_dom/index.js
@@ -0,0 +1,6 @@
+console.log("It is working for me when i did these settings.");
+
+/*Go to Developer tools → Settings → Console → tick "Selected context only". 
+The warnings will be hidden. You can see them again by unticking the same box.
+The "Selected context only" means only the top, iframe, worker and extension
+contexts. Which is all that you'll need, the vast majority of the time. */

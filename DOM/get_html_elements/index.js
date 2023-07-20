ea81b84577ecd76672789e87f37eb1870f372359
Ahmad Faraz <90551138+ahmadfaraz2@users.noreply.github.com>
2023-07-20 17:52:22 +0500

    Learn about get elements by Id, Class name and Tag name

---
+++ DOM/get_html_elements/index.js
@@ -0,0 +1,10 @@
+console.log("this is also working.");
+
+// Get Element By Id
+console.log(document.getElementById("topbar"));
+
+// Get Element by Class name
+console.log(document.getElementsByClassName("blog-card"));
+
+// Get Element by Tag name
+console.log(document.getElementsByTagName("img"));
